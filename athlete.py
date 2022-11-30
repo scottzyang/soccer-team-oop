4fd24430f42ba25043046fbb45d699debd8bf46b
Paul Fletes <paul.fletes@gmail.com>
2022-11-30 16:45:46 -0600

    add coach & defender classes

---
+++ athlete.py
@@ -0,0 +1,26 @@
+
+class Athlete:
+  def __init__(self, name, age, nation, yoe, salary):
+    self.name = name
+    self.age = age
+    self.nation = nation
+    self.yoe = yoe
+    self.salary = salary
+
+  def introduction(self):
+    print(f'Hello my name is {self.name}, I am {self.age} years old. I am from {self.nation}')
+
+  # possibly add another method
+  def contract_increase(self, percentage_increase):
+    increase = self.salary * (percentage_increase / 100)
+    self.salary += increase
+
+
+# test --------------------------------------------------------------------------------
+athlete = Athlete('Christian Pulisic', 24, 'USA', 8, 250000)
+print(athlete.name)
+
+# test salary method
+print(athlete.salary)
+athlete.contract_increase(30)
+print(athlete.salary) 
---
+++ coach.py
@@ -0,0 +1,34 @@
+from athlete import Athlete
+
+
+class Coach(Athlete):
+    def __init__(self, name, age, nation, yoe, salary, yrs_coaching, trophies, team_managing):
+        super().__init__(name, age, nation, yoe, salary)
+        self.yrs_coaching = yrs_coaching
+        self.trophies = trophies
+        self.team_managing = team_managing
+
+    def encourage_player(self):
+        print(f'Great pass, now make the run!')
+
+    def scold_player(self):
+        print(f"You should have made the pass!")
+
+
+# test --------------------------------------------------------------------------------
+Ancelotti = Coach('Carlo Ancelotti', 63, 'Italy', 42,
+                  10500000, 30, 23, 'Real Madrid')
+print(Ancelotti.name)
+
+# encourage player
+print(Ancelotti.yrs_coaching)
+Ancelotti.encourage_player()
+
+# scold player
+print(Ancelotti.trophies)
+Ancelotti.scold_player()
+
+# test inherited methods
+print(Ancelotti.salary)
+Ancelotti.contract_increase(10)
+print(Ancelotti.salary)
