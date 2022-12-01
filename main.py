8a94945b324cedbdbc2936c86e594c356eee2369
scottzyang <scottzyang@gmail.com>
2022-11-30 22:00:24 -0600

    Populate list of current roster

---
+++ main.py
@@ -0,0 +1,16 @@
+from attacker import Attacker
+from defender import Defender
+from freeagents import players
+from coach import Coach
+from team import Team
+
+# create current roster from free agents
+current_roster = list()
+for player in players:
+  if player['position'] == 'Defender':
+    new_player = Defender(player['name'], player['age'], player['nation'], player['salary'], player['number'],player['preferred_foot'], player['position'], player['in_box'])
+  else:
+    new_player = Attacker(player['name'], player['age'], player['nation'], player['salary'], player['number'],player['preferred_foot'], player['position'], player['skill_move'])
+  current_roster.append(new_player)
+
+print(current_roster)
