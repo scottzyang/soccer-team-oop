c7051b1c8e7d5dadf162edf33b41a509dd704964
scottzyang <scottzyang@gmail.com>
2022-11-30 21:59:57 -0600

    Moved player data to new file

---
+++ freeagents.py
@@ -0,0 +1,32 @@
+players = [
+  {
+    'name': 'Christian Pulisic',
+    'age': 24,
+    'nation': 'USA',
+    'salary': 650000,
+    'number': 10,
+    'preferred_foot': 'right',
+    'position': 'Attacker',
+    'skill_move': 'step over',
+  },
+  {
+    'name': 'Tim Ream',
+    'age': 35,
+    'nation': 'USA',
+    'salary': 120000,
+    'number': 3,
+    'preferred_foot': 'right',
+    'position': 'Defender',
+    'in_box': True,
+  },
+  {
+    'name': 'Tyler Adams',
+    'age': 23,
+    'nation': 'USA',
+    'salary': 750000,
+    'number': 4,
+    'preferred_foot': 'right',
+    'position': 'Attacker',
+    'skill_move': 'effective tackle',
+  },
+]
